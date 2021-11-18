e4b722a4cc9cbb309b2d4be7c1a7291729c2cba2
James Wilkins <jawilkin@redhat.com>
2021-11-18 13:58:01 +0000

    add remote svc

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+import requests
 
 app = Flask(__name__)
 api = Api(app)
@@ -19,9 +20,15 @@ class Counter(Resource):
         global count
         count += 1
         return {'Counter' : count}
+class FetchCount(Resource):
+    def get(self):
+        r = requests.get('http://count-svc:5000/count')
+        return {'Counter' : r.json().get('Counter')}
 api.add_resource(HelloWorld, '/')
 api.add_resource(SomeCrap, '/foo')
 api.add_resource(Counter, '/count')
+api.add_resource(FetchCount, '/fetch')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
 
