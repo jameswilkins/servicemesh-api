0d46aeb2bff9c1ea0db317a41ae1bb42dafe2d0c
James Wilkins <jawilkin@redhat.com>
2021-11-18 13:42:28 +0000

    add count func

--- api.py
+++ api.py
@@ -4,6 +4,8 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+count = 0
+
 class HelloWorld(Resource):
     def get(self):
         return {'a day my ': 'ass'}
@@ -12,9 +14,14 @@ class SomeCrap(Resource):
     def get(self):
         return {'Calling from ' : '/foo'}
 
+class Counter(Resource):
+    def get(self):
+        global count
+        count += 1
+        return {'Counter' : count}
 api.add_resource(HelloWorld, '/')
 api.add_resource(SomeCrap, '/foo')
-
+api.add_resource(Counter, '/count')
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
 
